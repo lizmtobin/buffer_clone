d1ea8b0056b6854edfdb99e5f8eb218e55b6cb06
liz tobin <lizmtobin@gmail.com>
2022-03-01 09:09:40 +0000

    add session cookie check

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
     def index
-        flash[:notice] = "logged in successfully"
-        flash[:alert] = "Log in unsuccessful"
+        if session[:user_id]
+            @user = User.find{session[:user_id]}
+        end
     end
 end
