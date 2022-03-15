ad449f57a7df3e7177d9f934977926e04d54219e
liz tobin <lizmtobin@gmail.com>
2022-03-15 16:54:37 +0000

    password mailer created

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,12 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:email])
+        if @user.present?
+            #send email
+            redirect_to root_path, notice: "If an account with that email account exists we will send you a reset link"
+        end
+        
     end
     
 end
