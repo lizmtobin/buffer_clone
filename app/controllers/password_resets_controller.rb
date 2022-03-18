acbe275892916bc2da573dd002455394ce5a7e65
liz tobin <lizmtobin@gmail.com>
2022-03-18 16:56:05 +0000

    pw edit view added

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,5 +11,9 @@ class PasswordResetsController < ApplicationController
         end
         
     end
+
+    def edit
+        @user = User.find_signed(params[:token], purpose:"reset_reset")
+    end
     
 end
