26df8d1b9716ab016047e2d878b6c8c8114301f1
liz tobin <lizmtobin@gmail.com>
2022-03-21 20:52:44 +0000

    spelling err

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,7 +12,7 @@ class PasswordResetsController < ApplicationController
     end
 
     def edit
-        @user = User.find_signed!(params[:token], purpose:"reset_reset")
+        @user = User.find_signed!(params[:token], purpose:"password_reset")
     rescue ActiveSupport::MessageVerifier::InvalidSignature 
         redirect_to sign_in_path, alert: "Your tocken has expired. Please try again."
     end
